f8667ccb770dbf6cff7747e2a6bec91691ea510a
Krzysztof Bogdan <krzysztof.bogdan@codeweaver.pl>
2016-09-30 17:00:59 +0200

    Rename in-out component to directive

--- app/banana-in-a-box/banana-in-a-box.module.ts
+++ app/banana-in-a-box/banana-in-a-box.module.ts
@@ -1,6 +1,6 @@
 import {NgModule} from "@angular/core";
 import {FormsModule} from "@angular/forms";
-import {InOutComponent} from "./in-out.component";
+import {InOutComponent} from "./in-out.directive";
 import {BananaInABoxComponentExample} from "./banana-in-a-box-example.component";
 
 @NgModule({
