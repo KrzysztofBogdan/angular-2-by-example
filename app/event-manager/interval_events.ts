30246cc3661358bc22cf42e96cbfa7df4371c169
Krzysztof Bogdan <krzysztof.bogdan@codeweaver.pl>
2016-09-30 14:03:10 +0200

    Change imports format

--- app/event-manager/interval_events.ts
+++ app/event-manager/interval_events.ts
@@ -1,14 +1,14 @@
-import * as ng from "@angular/core";
+import {Injectable, forwardRef} from "@angular/core";
 import {EVENT_MANAGER_PLUGINS, EventManager} from "@angular/platform-browser";
 
 
 export const INTERVAL_EVENTS_PLUGIN: any = {
     provide: EVENT_MANAGER_PLUGINS,
-    useClass: ng.forwardRef(() => IntervalPlugin),
+    useClass: forwardRef(() => IntervalPlugin),
     multi: true
 };
 
-@ng.Injectable()
+@Injectable()
 export class IntervalPlugin {
 
     manager: EventManager;
